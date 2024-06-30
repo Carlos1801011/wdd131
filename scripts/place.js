04670e92ea721ed432a1ac6d90a51b1a53e96035
carlos1801011 <cha21074@byui.edu>
2024-06-29 22:25:30 -0600

    b

--- scripts/place.js
+++ scripts/place.js
@@ -1,25 +1,12 @@
-const temp = parseInt(document.querySelector("#current-temp").textContent);
-const wind = parseInt(document.querySelector("#windspeed").textContent);
-const wc = document.querySelector("#windchill");
-
-function calculateWindchill(temp, wind) {
-    return Math.round(13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16));
+function windChill(temp, speed) {
+    if (temp <= 50 && speed > 3) {
+        return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+    } else {
+        return document.querySelector("#windChill").innerHTML = `N/A`;
+    }
 }
 
-if (temp <= 10 && wind > 4.8) {
-    wc.innerHTML = `${calculateWindchill(temp, wind)}&deg; C`;
-}
-else {
-    wc.innerHTML = "N/A";
-}
+let wind = windChill(49, 5);
+let finalResult = wind.toFixed(2);
 
-const flow = await startFlow(page);
-
-
-// This timespan will try to restore the page from the bfcache.
-// Problems restoring from the bfcache are surfaced in this report.
-await flow.startTimespan();
-await page.goto('https://example2.com');
-await page.goBack();
-await flow.endTimespan();
-
+document.querySelector("#windChill").innerHTML = finalResult;
